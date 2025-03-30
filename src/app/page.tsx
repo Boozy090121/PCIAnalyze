b9b2ca708e3962b227d2010d26e7c99f8f97fbba
Boozy090121 <132027776+Boozy090121@users.noreply.github.com>
2025-03-30 08:44:26 -0400

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import React from 'react';
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  redirect('/dashboard');
-}
--- src/workers/excel-processor.worker.ts
+++
@@ -1,2 +0,0 @@
-// This is a simplified worker file for the demo
-export {};
